4c9eca4b55d2c6e3d7f8ad6fb8275f6308964155
Daniil Kiselev <kiselev.danil201313@gmail.com>
2020-11-17 17:40:10 +0300

    Hide some features

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,7 +6,6 @@ async def set_default_commands(dp):
         types.BotCommand("instasave", "Скачать фото из Instagram"),
         types.BotCommand("steam", "Актуальные скидки Steam"),
         types.BotCommand("city", "Игра в города России"),
-        types.BotCommand("news", "Новости"),
         types.BotCommand("meme", "Пендосский мем"),
         types.BotCommand("joke", "Несмешные шутки"),
         types.BotCommand("weather", "Прогноз погоды"),
